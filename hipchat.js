b9f947ce75d82bca6ea606b2687e1148c11a760d
Inna <innakutsik@gmail.com>
2016-03-14 17:30:15 +0200

    add js

--- hipchat.js
+++ hipchat.js
@@ -12,13 +12,19 @@ var componentsCall = $.ajax('https://api.statuspage.io/v1/pages/' + PAGE_ID + '/
 $(function(){
   
   Promise.all([incidentsCall, componentsCall]).then(function(data){
-  	console.log(data)
+  	var today = new Date().getDate();
+  	var classTickTack = ['upwork'];
   	var incidents = data[0];
   	var components = data[1];
   	var ticks = [];
   	for(var i=1; i<32; i++){
-  		ticks.push({'i': i})
+  		if(i<=today){
+  			ticks.push({'i': i, 'classTick': classTickTack[0]})
+  		}else{
+  		ticks.push({'i': i, 'classTick': ''})
+  		}
   	}
+
   	var template = $('#incidentsTemplate').html();
   	var output = Mustache.render(template, {incidents: incidents, components: components, ticks: ticks});
   	 $('body').html(output);
