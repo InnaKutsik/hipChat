4815e482bcfcdf9bd2aa8cb4bfb90929b3c2a7d7
Inna <innakutsik@gmail.com>
2016-03-14 17:56:46 +0200

    add js

--- hipchat.js
+++ hipchat.js
@@ -12,10 +12,19 @@ var componentsCall = $.ajax('https://api.statuspage.io/v1/pages/' + PAGE_ID + '/
 $(function(){
   
   Promise.all([incidentsCall, componentsCall]).then(function(data){
-  	var today = new Date().getDate();
-  	var classTickTack = ['upwork'];
   	var incidents = data[0];
   	var components = data[1];
+  	console.log(incidents);
+  	var info = {
+  		'created': incidents[0]['created_at'],
+  		'status': incidents[0]['status'],
+  		'resolved': incidents[0]['resolved_at'],
+  		'investigating': incidents[0]['incident_updates'][1]['body'],
+  		'resolved_title': incidents[0]['incident_updates'][0]['body']
+  	}
+  	console.log(info)
+  	var today = new Date().getDate();
+  	var classTickTack = ['upwork'];
   	var ticks = [];
   	for(var i=1; i<32; i++){
   		if(i<=today){
@@ -26,7 +35,7 @@ $(function(){
   	}
 
   	var template = $('#incidentsTemplate').html();
-  	var output = Mustache.render(template, {incidents: incidents, components: components, ticks: ticks});
+  	var output = Mustache.render(template, {incidents: incidents, components: components, ticks: ticks, info: info});
   	 $('body').html(output);
 	});
 });
