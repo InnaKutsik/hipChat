331f23d2a1094afca95160df1edb75efd86719e7
Inna <innakutsik@gmail.com>
2016-03-30 21:46:45 +0300

    fix color

--- hipchat.js
+++ hipchat.js
@@ -55,63 +55,70 @@ $(function(){
     var infoIncident = getIncident.reverse();
     
 
-//     infoIncident = [{created: "2016-03-23T17:29:05.835+02:00",
-//         id: "hkc6cnpg9tqx",
-//         name: "Incident #4",
-//         planned_work: null,
-//         planned_work_created: null,
-//         planned_work_resolved: null,
-//         resolved: "2016-03-23T20:29:06.147+02:00",
-//         status: "resolved"
-//         }, {created: "2016-03-15T10:38:31.340+02:00",
-// id: "8svcgyb55xdp",
-// name: "Test maintenance 2",
-// planned_work: "2016-03-15T10:00:00.000+02:00",
-// planned_work_created: "2016-03-15T10:00:00.000+02:00",
-// planned_work_resolved: "2016-03-20T22:30:00.000+02:00",
-// resolved: "2016-03-23T22:30:41.878+02:00",
-// status: "completed"},{created: "2016-02-15T17:38:31.340+02:00",
-// id: "8svcgyb55xdp",
-// name: "Test maintenance 2",
-// planned_work: "2016-03-15T20:00:00.000+02:00",
-// planned_work_created: "2016-02-15T20:00:00.000+02:00",
-// planned_work_resolved: "2016-02-20T22:30:00.000+02:00",
-// resolved: "2016-02-23T22:30:41.878+02:00",
-// status: "completed"},{created: "2015-12-30T17:29:05.835+02:00",
-//         id: "hkc6cnpg9tqx",
-//         name: "Incident #4",
-//         planned_work: null,
-//         planned_work_created: null,
-//         planned_work_resolved: null,
-//         resolved: "2015-01-31T17:29:06.147+02:00",
-//         status: "resolved"
-//         }, {created: "2016-01-03T17:29:05.835+02:00",
-//         id: "hkc6cnpg9tqx",
-//         name: "Incident #4",
-//         planned_work: null,
-//         planned_work_created: null,
-//         planned_work_resolved: null,
-//         resolved: "2016-01-23T17:29:06.147+02:00",
-//         status: "resolved"
-//         }, {created: "2015-07-03T17:29:05.835+02:00",
-//         id: "hkc6cnpg9tqx",
-//         name: "Incident #4",
-//         planned_work: null,
-//         planned_work_created: null,
-//         planned_work_resolved: null,
-//         resolved: "2015-07-23T17:29:06.147+02:00",
-//         status: "resolved"
-//         }]
+    infoIncident = [{created: "2016-03-23T17:29:05.835+02:00",
+        id: "hkc6cnpg9tqx",
+        name: "Incident #4",
+        planned_work: null,
+        planned_work_created: null,
+        planned_work_resolved: null,
+        resolved: "2016-03-23T20:29:06.147+02:00",
+        status: "resolved"
+        }, {created: "2016-03-15T10:38:31.340+02:00",
+id: "8svcgyb55xdp",
+name: "Test maintenance 2",
+planned_work: "2016-03-15T10:00:00.000+02:00",
+planned_work_created: "2016-03-15T10:00:00.000+02:00",
+planned_work_resolved: "2016-03-20T22:30:00.000+02:00",
+resolved: "2016-03-23T22:30:41.878+02:00",
+status: "completed"},{created: "2016-02-15T17:38:31.340+02:00",
+id: "8svcgyb55xdp",
+name: "Test maintenance 2",
+planned_work: "2016-03-15T20:00:00.000+02:00",
+planned_work_created: "2016-02-15T20:00:00.000+02:00",
+planned_work_resolved: "2016-02-20T22:30:00.000+02:00",
+resolved: "2016-02-23T22:30:41.878+02:00",
+status: "completed"},{created: "2015-12-30T17:29:05.835+02:00",
+        id: "hkc6cnpg9tqx",
+        name: "Incident #4",
+        planned_work: "2015-12-30T17:29:05.835+02:00",
+        planned_work_created: "2015-12-30T17:29:05.835+02:00",
+        planned_work_resolved: "2016-01-02T17:29:06.147+02:00",
+        resolved: "2016-01-02T17:29:06.147+02:00",
+        status: "resolved"
+        }, {created: "2016-01-03T17:29:05.835+02:00",
+        id: "hkc6cnpg9tqx",
+        name: "Incident #4",
+        planned_work: null,
+        planned_work_created: null,
+        planned_work_resolved: null,
+        resolved: "2016-01-23T17:29:06.147+02:00",
+        status: "resolved"
+        }, {created: "2016-01-31T17:29:05.835+02:00",
+        id: "hkc6cnpg9tqx",
+        name: "Incident #4",
+        planned_work: null,
+        planned_work_created: null,
+        planned_work_resolved: null,
+        resolved: "2016-02-02T17:29:06.147+02:00",
+        status: "resolved"
+        }, {created: "2015-07-03T17:29:05.835+02:00",
+        id: "hkc6cnpg9tqx",
+        name: "Incident #4",
+        planned_work: null,
+        planned_work_created: null,
+        planned_work_resolved: null,
+        resolved: "2015-07-23T17:29:06.147+02:00",
+        status: "resolved"
+        }]
 
     var getYear = function(){
-      var year = new Date().getFullYear()
+      var date = new Date().getTime()
       for(var i=0; i<infoIncident.length; i++){
-        if(infoIncident[i]['created'] || infoIncident[i]['planned_work_created']){
-          var creat = (new Date(Date.parse(infoIncident[i]['created']))).getFullYear() || (new Date(Date.parse(infoIncident[i]['planned_work_created']))).getFullYear()
-          if(year>creat) year = creat;
-        }
+        var eventDate = Date.parse(infoIncident[i]['created'])
+        if(date>eventDate) date = eventDate;
       }
-      return year;
+      console.log([new Date(date).getFullYear(), new Date(date).getMonth()])
+      return [new Date(date).getFullYear(), new Date(date).getMonth()];
     }
     
     function getLastDayOfMonth(year, month) {
@@ -121,7 +128,7 @@ $(function(){
 
     function makeMonthTicks(date){
       var tick=[]
-      if(date.getMonth()==new Date().getMonth() && date.getYear()===new Date().getYear()){
+      if(date.getMonth()==new Date().getMonth() && date.getFullYear()===new Date().getFullYear()){
         for(var i=1; i<32; i++){
           if(i<=today.getDate()){
             tick.push({'i': i, 'classTick': classTickTack[0]['cls'], 'numberOfTick': 'tick'+i})
@@ -131,7 +138,7 @@ $(function(){
         }
       }else{
         for(var i=1; i<32; i++){
-          if(i<=getLastDayOfMonth(date.getYear(), date.getMonth())){
+          if(i<=getLastDayOfMonth(date.getFullYear(), date.getMonth())){
             tick.push({'i': i, 'classTick': classTickTack[0]['cls'], 'numberOfTick': 'tick'+i})
           }else{
             tick.push({'i': i, 'classText': 'unactive' , 'numberOfTick': 'tick'+i})
@@ -161,18 +168,23 @@ $(function(){
             }
             months.push(createTicks(currentMonth));
           }
-      } else{
+      }else if(date.getFullYear() != getYear()[0]){
         for(var i=11; i>=0; i--){
           var currentMonth = new Date(date.setMonth(i));
           months.push(createTicks(currentMonth));
         }
+      }else{
+        console.log(getYear()[1])
+        for(var i=11; i>=getYear()[1]; i--){
+          var currentMonth = new Date(date.setMonth(i));
+          months.push(createTicks(currentMonth));
+        }
       }
-      console.log(months)
       return months;
     }
     function makeYear(date){
       var year=date.getFullYear()
-      var length = year - getYear()
+      var length = year - getYear()[0]
       var years = []
       for(var i=0; i<=length; i++){
         var currentYear = new Date(date.setFullYear(year - i));
@@ -195,11 +207,11 @@ $(function(){
     //function to get json by month
     function makeMonthsEvents(date){
       var year=date.getFullYear()
-      var length = year - getYear()
+      var length = year - getYear()[0]
       var years = []
       var month=date.getMonth()
       for(var j=0; j<=length; j++){
-        for(var i=month; i>=0; i--){
+        for(var i=13; i>=0; i--){
           var eventData = getPerMonth(new Date(new Date(date.getFullYear()-j, i, 1)), infoIncident);
           addShaduleWork(eventData);
           addIncident(eventData);
@@ -223,8 +235,6 @@ $(function(){
               for(var j=0; j<=countDay; j++){
                 var creat = new Date(Date.parse(data[t]['created']));
                 var creatShift = new Date(creat.setDate(creat.getDate()+j));
-
-                console.log(creatShift)
                 if(j==0){
                   $("."+ yearEvent(data[t]['created']) +" .month"+ monthEvent(data[t]['created']) + " .tick"+eventDay).parent().append('<li style="'+gradient(createdDate, classTickTack[1]['color'])+' z-index: 20;" class="tick-tacks" ></li>');
                 } else if(j<(countDay)){
@@ -251,12 +261,14 @@ $(function(){
                 $("."+ yearEvent(data[t]['planned_work_created']) +" .month"+ monthEvent(data[t]['planned_work_created']) + " .tick"+eventDay).parent().append('<li style="'+gradient(createdDate, classTickTack[2]['color'], resolvedDate)+'" class="tick-tacks"></li>');
               } else {
                 for(var j=0; j<=countDay; j++){
+                  var creat = new Date(Date.parse(data[t]['planned_work_created']));
+                  var creatShift = new Date(creat.setDate(creat.getDate()+j));
                   if(j==0){
                     $("."+ yearEvent(data[t]['planned_work_created']) +" .month"+ monthEvent(data[t]['planned_work_created']) + " .tick"+eventDay).parent().append('<li style="'+gradient(createdDate, classTickTack[2]['color'])+'" class="tick-tacks"></li>');
                   } else if(j<(countDay)){
-                      $("."+ yearEvent(data[t]['planned_work_created']) +" .month"+ monthEvent(data[t]['planned_work_created']) + " .tick"+(eventDay+j)).parent().append('<li style="'+gradient(0, classTickTack[2]['color'], 0)+'" class="tick-tacks"></li>');
+                      $("."+ creatShift.getFullYear() +" .month"+ creatShift.getMonth() + " .tick"+ creatShift.getDate()).parent().append('<li style="'+gradient(0, classTickTack[2]['color'], 0)+'" class="tick-tacks"></li>');
                   }else{
-                      $("."+ yearEvent(data[t]['planned_work_created']) +" .month"+ monthEvent(data[t]['planned_work_created']) + " .tick"+(eventDay+j)).parent().append('<li style="'+gradient(0, classTickTack[2]['color'], resolvedDate)+'" class="tick-tacks"></li>');
+                      $("."+ creatShift.getFullYear() +" .month"+ creatShift.getMonth() + " .tick"+ creatShift.getDate()).parent().append('<li style="'+gradient(0, classTickTack[2]['color'], resolvedDate)+'" class="tick-tacks"></li>');
                     }
                 }
               }
